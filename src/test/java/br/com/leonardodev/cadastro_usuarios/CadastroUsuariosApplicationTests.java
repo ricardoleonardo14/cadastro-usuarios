fbcfb7aa2e1a7d9a1a8f3fedae0897b1e993ec25
ricardoleonardo14 <leonardoricardo83@gmail.com>
2024-10-08 22:03:58 -0300

    first commit

---
+++ src/test/java/br/com/leonardodev/cadastro_usuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardodev.cadastro_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
